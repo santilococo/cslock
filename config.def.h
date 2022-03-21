2ea909a8d21313d8dfee05551e34715b4538362c
Santiago Lo Coco <santilococo.01@gmail.com>
2022-03-21 15:31:53 -0300

    Remove comments

--- config.def.h
+++ config.def.h
@@ -14,25 +14,11 @@ static const int failonclear = 1;
 /* insert grid pattern with scale 1:1, the size can be changed with logosize */
 static const int logosize = 75;
 /* grid width and height for right center alignment */
-//static const int logow = 12;
 static const int logow = 14;
 static const int logoh = 8;
 
 static XRectangle rectangles[14] = {
 	/* x    y       w       h */
-    /* DWM LOGO
-	{ 0,    3,      1,      3 },
-	{ 1,    3,      2,      1 },
-	{ 0,    5,      8,      1 },
-	{ 3,    0,      1,      5 },
-	{ 5,    3,      1,      2 },
-	{ 7,    3,      1,      2 },
-	{ 8,    3,      4,      1 },
-	{ 9,    4,      1,      2 },
-	{ 11,   4,      1,      2 },
-	*/
-
-    // I used https://www.khanacademy.org/computer-programming/rectx-y-width-height-radius/839496660 (but this values multiplied by 10)
 	{ 0,	3,	1,	3 },
 	{ 1,	3,	2,	1 },
 	{ 1,	5,	2,	1 },
