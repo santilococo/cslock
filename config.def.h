14e89747cfc9824c3c1b9ee7c8ae21e265903e36
Santiago Lo Coco <santilococo.01@gmail.com>
2022-03-21 15:30:07 -0300

    Add custom logo

--- config.def.h
+++ config.def.h
@@ -14,11 +14,13 @@ static const int failonclear = 1;
 /* insert grid pattern with scale 1:1, the size can be changed with logosize */
 static const int logosize = 75;
 /* grid width and height for right center alignment */
-static const int logow = 12;
-static const int logoh = 6;
+//static const int logow = 12;
+static const int logow = 14;
+static const int logoh = 8;
 
-static XRectangle rectangles[9] = {
+static XRectangle rectangles[14] = {
 	/* x    y       w       h */
+    /* DWM LOGO
 	{ 0,    3,      1,      3 },
 	{ 1,    3,      2,      1 },
 	{ 0,    5,      8,      1 },
@@ -28,6 +30,26 @@ static XRectangle rectangles[9] = {
 	{ 8,    3,      4,      1 },
 	{ 9,    4,      1,      2 },
 	{ 11,   4,      1,      2 },
+	*/
+
+    // I used https://www.khanacademy.org/computer-programming/rectx-y-width-height-radius/839496660 (but this values multiplied by 10)
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 1,	5,	2,	1 },
+
+	{ 4,	3,	1,	3 },
+	{ 5,	3,	1,	1 },
+	{ 5,	5,	1,	1 },
+	{ 6,	3,	1,	3 },
+
+	{ 8,	3,	1,	3 },
+	{ 9,	3,	2,	1 },
+	{ 9,	5,	2,	1 },
+
+	{ 12,	3,	1,	3 },
+	{ 13,	3,	1,	1 },
+	{ 13,	5,	1,	1 },
+	{ 14,	3,	1,	3 },
 };
 
 /*Enable blur*/
